83cf58742f3c362b39fe62ab28485e3885b40f58
Crow-III <RobertRuffin70@yahoo.com>
2023-08-18 22:37:03 -0500

    major overhual to game

--- script.js
+++ script.js
@@ -1,77 +1,107 @@
-const startButton = document.getElementById('START')
-const nextButton = document.getElementById('NEXT')
-const questionContainerElement = document.getElementById('Questions')
-const questionElement = document.getElementById('questions?')
-const answerButtonsElement = document.getElementById('answerButtons')
-let shuffledQuestions, currentQuestionIndex
+const startButton = document.getElementById('START');
+const questionContainerElement = document.getElementById('Questions');
+const questionElement = document.getElementById('questions?');
+const answerButtonsElement = document.getElementById('answerButtons');
+let shuffledQuestions, currentQuestionIndex;
+let Time;
+let sec = 60;
 
+startButton.addEventListener('click', startGame);
 
-startButton.addEventListener('click', startGame)
-
-
-// Timer for quiz not complete
-var Time
-var Timer = document.getElementById('Timer');
-
-(function () {
-var sec = 0; 
-Time = setInterval (()=>{
-Timer.innerText = '60:'})
-})
-
-//will start quiz
 function startGame() {
-startButton.classList.add('hide')
-shuffledQuestions = questions.sort(()=> Math.random() - .5)
-questionContainerElement.classList.remove('hide')
-currentQuestionIndex = 0
-setNextQuestion()
+    startButton.classList.add('hide');
+    shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+    questionContainerElement.classList.remove('hide');
+    currentQuestionIndex = 0;
+    setNextQuestion();
+
+    // Start the timer when the game starts
+    Time = setInterval(() => {
+        document.getElementById('Timer').innerText = '00:' + (sec < 10 ? '0' : '') + sec;
+        sec--;
+        if (sec < 0) {
+            clearInterval(Time);
+            document.getElementById('Timer').innerText = 'Time Out';
+        }
+    }, 1000);
 }
+
 
 
 function setNextQuestion() {
-resetState()
-showQuestion(shuffledQuestions[currentQuestionIndex])
-
-}
-
-
-function showQuestion(question) {
-    questionElement.innerText = question.questions
-    question.answers.forEach(answers => {
-        const button = document.createElement('button')
-        button.innerText = answers.text
-        button.classList.add('button')
-        if (answers.correct) {
-            button.dataset.correct = answers.correct
-        }
-        button.addEventListener('click', selectAnswer)
-        answerButtonsElement.appendChild(button)
-    })
-}
-
-function resetState() {
-    nextButton.classList.add('hide')
-    while (answerButtonsElement.firstChild) {
-        answerButtonsElement.removeChild
-        (answerButtonsElement.firstChild)
-        
-        
+    resetState();
+    if (currentQuestionIndex < questions.length) {
+        showQuestion(shuffledQuestions[currentQuestionIndex]);
+    } else {
+        // All questions are answered, end the game here
+        clearInterval(Time);
+        document.getElementById('Timer').innerText = 'Game Over';
     }
 }
 
-function selectAnswer() {
 
+
+function showQuestion(question) {
+    questionElement.innerText = question.questions;
+    question.answers.forEach((answers) => {
+        const button = document.createElement('button');
+        button.innerText = answers.text;
+        button.classList.add('button');
+        if (answers.correct) {
+            button.dataset.correct = answers.correct;
+        }
+        button.addEventListener('click', selectAnswer);
+        answerButtonsElement.appendChild(button);
+    });
+}
+
+function resetState() {
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild);
+    }
+}
+
+function selectAnswer(event) {
+    const selectedButton = event.target;
+    const correct = selectedButton.dataset.correct;
+
+    if (!correct) {
+        sec -= 10; // Reduce 10 seconds for wrong answer
+    }
+
+    // You can add other logic here to handle the selected answer, e.g., highlighting correct/wrong answers
+
+    // Move to the next question
+    currentQuestionIndex++;
+    setNextQuestion();
 }
 
 const questions = [
     {
         questions: 'what is the element p1 for in HTML',
         answers: [
-            {text: 'paragraphs', correct: true },
-            {text: 'Headers', correct: false},
-            {text: 'Titles', correct: false},
-            {text: 'Footers', correct: false}
-        ]
-    }
-]
+            { text: 'paragraphs', correct: true },
+            { text: 'Headers', correct: false },
+            { text: 'Titles', correct: false },
+            { text: 'Footers', correct: false },
+        ],
+    },
+    {
+        questions: 'What is mainly used to style websites',
+        answers: [
+            { text: 'CSS', correct: true },
+            { text: 'HTML', correct: false },
+            { text: 'javascript', correct: false },
+            { text: 'none of the above', correct: false },
+        ],
+    },
+    {
+        questions: 'what is javascript considered when linking together webpages',
+        answers: [
+            { text: 'brain', correct: true },
+            { text: 'foot', correct: false },
+            { text: 'mouth', correct: false },
+            { text: 'hands', correct: false },
+        ],
+    },
+];
